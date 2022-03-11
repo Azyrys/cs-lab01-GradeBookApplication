e010ad9a2080a4ec8fb3570e1c9573461df5dd08
Piotr Stalmach <piotr_stalmach@outlook.com>
2022-03-11 21:21:46 +0100

    Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -8,5 +8,24 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5) throw new InvalidOperationException();
+            int scoredHigher = 0;
+            foreach (Student student in Students)
+                if (student.AverageGrade > averageGrade)
+                    scoredHigher++;
+            double scoredHigherPercent = (double) scoredHigher / (double) Students.Count;
+            if (scoredHigherPercent < 0.2)
+                return 'A';
+            else if (scoredHigherPercent < 0.4)
+                return 'B';
+            else if (scoredHigherPercent < 0.6)
+                return 'C';
+            else if (scoredHigherPercent < 0.8)
+                return 'D';
+            return 'F';
+        }
     }
 }
